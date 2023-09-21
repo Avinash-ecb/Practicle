9d6f62d408390e057a5e532b0411d01083ea3ae1
Avinash Koshti <46588125+Avinash-ecb@users.noreply.github.com>
2023-09-21 11:39:47 +0530

    Add files via upload

---
+++ customer.js
@@ -0,0 +1,92 @@
+[
+  {
+    UserId: 1,
+    FN: "Arjun",
+    LN: "Gopal",
+    Phone: "9874563210",
+    Email: "arjun@gmail.com",
+    Gender: "Male",
+    City: "Surat"
+  },
+  {
+    UserId: 2,
+    FN: "Rahul",
+    LN: "Kumar",
+    Phone: null,
+    Email: "rahul@gmail.com",
+    Gender: "Male",
+    City: "Surat"
+  },
+  {
+    UserId: 3,
+    FN: "Aakash Rameshbhai",
+    LN: "Tanwani",
+    Phone: "9898287089",
+    Email: "vinpahilincometax1@gmail.com",
+    Gender: "Male",
+    City: "Rajkot"
+  },
+  {
+    UserId: 4,
+    FN: "Abhijeet",
+    LN: "Chopra",
+    Phone: "9558709695",
+    Email: "abhijeet@gmail.com",
+    Gender: "Male",
+    City: "Bhuj"
+  },
+  {
+    UserId: 5,
+    FN: "Aditi",
+    LN: "Thapar",
+    Phone: "9558796095",
+    Email: "thapar@gmail.com",
+    Gender: "Female",
+    City: "Bhuj"
+  },
+  {
+    UserId: 6,
+    FN: "DECOR",
+    LN: "AAIJI ",
+    Phone: "7425896095",
+    Email: "aaiji@gmail.com",
+    Gender: "Male",
+    City: "Surat"
+  },
+  {
+    UserId: 7,
+    FN: "Hardik",
+    LN: "Patel",
+    Phone: "6353695363",
+    Email: "hardik@gmail.com",
+    Gender: "Male",
+    City: "Surat"
+  },
+  {
+    UserId: 8,
+    FN: "Harsh",
+    LN: "Fammeo",
+    Phone: "7359938113",
+    Email: "fammoe@gmail.com",
+    Gender: "Male",
+    City: "Surat"
+  },
+  {
+    UserId: 9,
+    FN: "Anjali",
+    LN: "Patel",
+    Phone: "951236470",
+    Email: "anjali@gmail.com",
+    Gender: "Male",
+    City: "navsari"
+  },
+  {
+    UserId: 10,
+    FN: "Avinash",
+    LN: "Koshti",
+    Phone: "7558796095",
+    Email: "koshti@gmail.com",
+    Gender: "Male",
+    City: "Surat"
+  }
+]
